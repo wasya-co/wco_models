4d94c4f2eb2a07282fb50094dcf3bbe45f28118b
piousbox <piousbox@gmail.com>
2017-06-02 15:01:36 -0500

    galleries, reports are private by default

--- lib/app_model2.rb
+++ lib/app_model2.rb
@@ -3,7 +3,7 @@ class AppModel2
   include ::Mongoid::Timestamps
 
   field :is_feature, :type => Boolean, :default => false
-  field :is_public, :type => Boolean, :default => true
+  field :is_public, :type => Boolean, :default => false
   field :is_done, :type => Boolean, :default => false
   field :is_trash, :type => Boolean, :default => false
   field :is_anonymous, :type => Boolean, :default => false

d0837b6507417b437c7dfda01fa0881cedc5c421
mac_a2141 <victor@piousbox.com>
2023-04-02 13:56:04 -0500

    sch#send_and_roll

--- lib/office/scheduled_email_action.rb
+++ lib/office/scheduled_email_action.rb
@@ -28,6 +28,33 @@ class Office::ScheduledEmailAction
 
   field :perform_at, type: :time
 
+  def send_and_roll
+    sch = self
+    sch.update_attributes({ state: Sch::STATE_INACTIVE })
+
+    # send now
+    ctx = Ctx.create!({
+      email_template_id: sch.act.tmpl.id,
+      lead_id: sch.lead.id,
+      send_at: Time.now,
+      subject: sch.act.tmpl.subject,
+      from_email: sch.act.tmpl.from_email,
+      scheduled_email_action_id: sch.act.id,
+    })
+
+    # schedule next actions & update the action
+    sch.act.ties.each do |tie|
+      next_act = tie.next_email_action
+      next_at  = eval(tie.next_at_exe)
+      next_sch = Sch.find_or_initialize_by({
+        lead_id: sch.lead_id,
+        email_action_id: next_act.id,
+      })
+      next_sch.perform_at = next_at
+      next_sch.state      = Sch::STATE_ACTIVE
+      next_sch.save!
+    end
+  end
 
 end
 ::Sch = Office::ScheduledEmailAction

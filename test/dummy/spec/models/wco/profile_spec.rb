cd7cf62d132b0a449e0f8e729bb4c6a3524a0c6e
mac_a2141 <victor@piousbox.com>
2023-12-16 17:57:20 -0500

    autobuild `date "+%Y%m%d"`

--- test/dummy/spec/models/wco/profile_spec.rb
+++ test/dummy/spec/models/wco/profile_spec.rb
@@ -3,7 +3,10 @@
 RSpec.describe Wco::Profile, type: :model do
 
   it 'sanity' do
-    m = Wco::Profile.create!
+    Wco::Profile.all.destroy_all
+
+    m = Wco::Profile.create( email: 'test@email.com' )
+
     m.persisted?.should eql true
   end
 

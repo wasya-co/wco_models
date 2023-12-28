8c7ddb50febc2ed48496a49fa6d781b3f466320e
mac_a2141 <victor@piousbox.com>
2023-12-27 20:01:13 -0500

    autobuild `date "+%Y%m%d"`

--- app/assets/javascripts/wco/application.js
+++ app/assets/javascripts/wco/application.js
@@ -1,8 +1,9 @@
 //
-//= require ../vendor/jquery-3.3.1.min
-//= require ../vendor/popper-1.14.0.min
-//= require ../vendor/bootstrap-4.6.2.min
-//= require ../vendor/fontawesome-5.15.4.min
+//= require vendor/test
+//= require vendor/jquery-3.3.1.min
+//= require vendor/popper-1.14.0.min
+//= require vendor/bootstrap-4.6.2.min
+//= require vendor/fontawesome-5.15.4.min
 //
 //= require ./collapse-expand
 //

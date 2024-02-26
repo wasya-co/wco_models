51ef1954c7befaea13395524e24cfda97785f1a4
mac_a2141 <victor@piousbox.com>
2024-02-26 08:25:49 -0600

    autobuild `date "+%Y%m%d"`

--- app/models/wco_email/conversation.rb
+++ app/models/wco_email/conversation.rb
@@ -26,7 +26,7 @@ class WcoEmail::Conversation
   field :preview, default: ''
 
   has_many :messages,             class_name: '::WcoEmail::Message'
-  default_scope ->{ includes(:messages) }
+  # default_scope ->{ includes(:messages) }
 
   has_and_belongs_to_many :tags,  class_name: 'Wco::Tag', index: true
   has_and_belongs_to_many :leads, class_name: 'Wco::Lead', index: true

7ac399d6bbad9d369ca8a8dcf326fc1808d90c38
mac_a2141 <victor@piousbox.com>
2026-01-01 17:12:42 -0600

    autobuild `date "+%Y%m%d"`

--- app/models/wco/newsvideo.rb
+++ app/models/wco/newsvideo.rb
@@ -137,7 +137,7 @@ AOL
     n = nil
     nn.each_with_index do |ms, idx|
       n = idx+1
-      ffmpeg_cmd.push "[#{curr_s}][v#{n}]overlay=0:0:eof_action=pass[tmp#{n}]; \\"
+      ffmpeg_cmd.push "[#{curr_s}][v#{n}]overlay=0:0:eof_action=pass[tmp#{n}]#{idx+1<nn.length ? ';' : ''} \\"
       curr_s = "tmp#{n}"
     end
     ffmpeg_cmd.push " \" -map \"[#{curr_s}]\" -map 0:a? -c:v libx264 -c:a copy combined_fin.mp4 "

4afddad9feed8382eb701da8de95f1a6b5d7d193
mac_a2141 <victor@piousbox.com>
2026-07-10 17:09:39 -0500

    hey it works

--- test/dummy/spec/models/wco_email/message_stub_spec.rb
+++ test/dummy/spec/models/wco_email/message_stub_spec.rb
@@ -10,36 +10,36 @@ RSpec.describe WcoEmail::MessageStub do
         WcoEmail::Conversation,
         WcoEmail::EmailFilter,
         WcoEmail::EmailTemplate,
+        WcoEmail::Message,
         WcoEmail::MessageStub,
       );
-      # @conv = create(:email_conversation)
-      # @leadset = create(:leadset, email: 'MAILER-DAEMON@amazonses.com' )
-      # @not_spam = Wco::Tag.create!({ slug: 'not-spam' })
-      # @email_template = create(:email_template)
+      @conv = create(:email_conversation)
+      @lead = create(:lead)
     end
 
     it 'Adds conv to this filter' do
+      filter  = create(:email_filter)
+      faction = create(:email_filter_action, email_filter: filter)
+      fcond   = create(:email_filter_condition, field: 'from', operator: 'match-i', value: 'pudeyev', email_filter: filter )
+      filter.conversation_ids.should eql([])
+
       stub   = create( :message_stub,
         bucket: ::SES_S3_BUCKET,
         object_key: '00nn652jk1395ujdr3l11ib06jam0oevjqv2o4g1' )
       stub.do_process_json
 
-      message = create(:email_message, conversation: @conv, lead: @lead )
-      filter  = create(:email_filter)
-      faction = create(:email_filter_action, email_filter: filter)
-
       filter.reload
-      filter.conversation_ids.should eql([ @conv.id ])
+      filter.conversation_ids.length.should eql 1
     end
 
     it 'populates preview' do
-      destroy_every( WcoEmail::Conversation )
-      stub = create( :message_stub, bucket: ::SES_S3_BUCKET, object_key: '00nn652jk1395ujdr3l11ib06jam0oevjqv2o4g1' )
+      stub   = create( :message_stub,
+        bucket: ::SES_S3_BUCKET,
+        object_key: '00nn652jk1395ujdr3l11ib06jam0oevjqv2o4g1' )
+      stub.do_process_json
 
-      stub.do_process
-
-      conversation = WcoEmail::Conversation.all.first
-      conversation.preview.should eql "Delivery has failed to these recipients or groups: russelldaveggio@hotmail.com The recipient's mailbox is full and can't accept messages now. Please try resending your message later, or contact the rec"
+      conversation = WcoEmail::Conversation.all.last
+      conversation.preview[0...100].should eql "logs:root@annesque:/opt/projects/annesque_email#root@annesque:/opt/projects/annesque_email#root@anne"
     end
 
   end
